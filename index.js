f5eaa85c1cbf7f69c3c48403683d005100146e8f
Daniil191245 <131523169+Daniil191245@users.noreply.github.com>
2024-07-14 11:50:58 +0300

    feat: update classwork

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // split() і join()
 
 
-const message = "Ви перемо`гли, ви молодець";
+const message = "Ви перемогли, ви молодець";
 // split - розділити
 // розділяє строку та збирає значення в масив
 console.log(message.split(","));
@@ -37,14 +37,58 @@ function indexOff(arr, index) {
 }
 
 
+
+
+
+
+
 // методи push(), pop(), shift(), unshift()
 
 // метод push()
-const arr2 = [1, 2, 3, 4, 5, 6 ,90, 10];
+const arr2 = [2, 3, 4, 5, 6, 90, 10];
 arr2.push(30);
 console.log(arr2);
 console.log(arr2.push(20));
 // повертає в консоль індекс елементу якого ми додали
 // метод push() - додає елемент у кінець маисву
 
-// pop();
+// pop(); 
+// pop() - видаляє елемент з кінця масива
+arr2.pop(10)
+console.log(arr2);
+console.log(arr2.pop(10));
+
+// shift()
+// shift() - видаляє перший елемент з масиву
+
+arr2.shift()
+console.log(arr2);
+console.log(arr2.shift());
+
+
+// unshift()
+// unshift() - додає елемент в початок масиву
+// додає будь-яку к-ть елементів на початок масиву, повертає довжину нового масиву
+arr2.unshift(1, 2, 3, 4, 5)
+console.log(arr2);
+console.log(arr2.unshift(1, 2, 3, 4, 5));
+
+// slice()
+// slice() - робить копію масиву
+// перший параметр - це індекс з якого має початися вирізання(start)
+// другий параметр - це індекс кінця, де завершити копіювання(end)
+const arr2Copy = arr2.slice(0, arr2.length);
+console.log(arr2Copy);
+// копіюємо масив, щоб зберегти дані які ми отримали з БД недоторканим
+const arr2CopyCopy = arr2.slice(2, 5);
+console.log(arr2CopyCopy);
+
+const winners = arr2.indexOf(90);
+
+
+// splice()
+const arr2Splice = arr2Copy.splice(0, arr2Copy.length, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+console.log(arr2Copy);
+console.log(arr2Splice);
+
+// concat() - об'єднує декілька масивів в один
